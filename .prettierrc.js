cc5ebf1c3ee934e93d8b32044553ecea864d64ca
leungkingto <jingtao.820@163.com>
2020-05-21 09:58:54 +0800

    prettier script

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 80,
+	useTabs: true,
+	tabWidth: 2,
+	singleQuote: true,
+	semi: false,
+	jsxBracketSameLine: true,
+	bracketSpacing: false,
+	vueIndentScriptAndStyle: false
+}
